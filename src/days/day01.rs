81b1a0b39481d0ed18168023ccd6ffcc4f07c910
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-12-03 20:06:09 +0100

    Optimize day 1
    Since the two vectors are already sorted for part 1, we don't need a hash table
    to get the frequencies in part two. This is now instead done in a single pass
    over both sorted vecs.

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,14 +1,37 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+
+struct SameIter<'a> {
+    next: &'a [i64],
+}
+
+impl Iterator for SameIter<'_> {
+    type Item = (usize, i64);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let fst = if let Some(x) = self.next.first() {
+            *x
+        } else {
+            return None;
+        };
+        unsafe {
+            // Safety: We just checked there is at least one element
+            let rest = self.next.split_at_unchecked(1).1;
+            let first_diff = rest.iter().position(|&i| i != fst).unwrap_or(rest.len());
+            // Safety: The result of `position` is always in [0, self.len()), and the alternative value
+            // of rest.len() is also inbounds.
+            self.next = rest.split_at_unchecked(first_diff).1;
+            Some((first_diff + 1, fst))
+        }
+    }
+}
 
 pub fn solve(s: &str) -> (i64, i64) {
-    let mut counts: HashMap<i64, i64> = HashMap::new();
     let (mut left, mut right) = s
         .lines()
         .map(|line| {
             let (a, b) = line.split_once("   ").unwrap();
             let a = a.parse::<i64>().unwrap();
             let b = b.parse::<i64>().unwrap();
-            *counts.entry(b).or_insert(0) += 1;
             (a, b)
         })
         .collect::<(Vec<_>, Vec<_>)>();
@@ -19,9 +42,26 @@ pub fn solve(s: &str) -> (i64, i64) {
         .zip(right.iter())
         .map(|(i, j)| (i - j).abs())
         .sum();
-    let p2 = left
-        .iter()
-        .fold(0, |acc, i| acc + i * counts.get(i).unwrap_or(&0));
+    let mut lefts = SameIter { next: &left };
+    let mut rights = SameIter { next: &right };
+    let mut elem_left = lefts.next();
+    let mut elem_right = rights.next();
+    let mut p2 = 0;
+    while let (Some((ln, lx)), Some((rn, rx))) = (elem_left, elem_right) {
+        match lx.cmp(&rx) {
+            Ordering::Less => {
+                elem_left = lefts.next();
+            }
+            Ordering::Greater => {
+                elem_right = rights.next();
+            }
+            Ordering::Equal => {
+                p2 += lx * (ln * rn) as i64;
+                elem_left = lefts.next();
+                elem_right = rights.next();
+            }
+        }
+    }
     (p1, p2)
 }
 
